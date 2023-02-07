753333315f9f508cccff2611cfcd9186da2d3774
Dmyto Kozubenko <kozubenko.dmitriy@gmail.com>
2023-02-07 21:11:02 +0100

    Added test user name without fixture

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,5 +1,19 @@
-def test_check_math():
-    assert 7 * 7 == 49
+class User:
 
-def test_check_78():
-    assert 7 * 8 == 56
+    def __init__(self) -> None:
+        self.name = "Sergii"
+        self.second_name = "Butenko"
+
+user = User()
+
+def test_remove_name():
+    user.name = ''
+    assert user.name == ''
+
+
+def test_name():
+    assert user.name == "Sergii"
+
+
+def test_second_name():
+    assert user.second_name == "Butenko"
