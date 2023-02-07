10e6b2444753b79fe7bf2430c5f23d84fcf67cf5
Dmyto Kozubenko <kozubenko.dmitriy@gmail.com>
2023-02-07 21:14:10 +0100

    Added test with fixture

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,19 +1,26 @@
+import pytest
+
+
 class User:
 
     def __init__(self) -> None:
-        self.name = "Sergii"
-        self.second_name = "Butenko"
+        self.name = "Dmytro"
+        self.second_name = "Kozubenko"
 
-user = User()
 
-def test_remove_name():
+@pytest.fixture
+def user():
+    yield User()
+
+
+def test_remove_name(user):
     user.name = ''
     assert user.name == ''
 
 
-def test_name():
-    assert user.name == "Sergii"
+def test_name(user):
+    assert user.name == "Dmytro"
 
 
-def test_second_name():
-    assert user.second_name == "Butenko"
+def test_second_name(user):
+    assert user.second_name == "Kozubenko"
