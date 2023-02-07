2a0280398949c615989d79e983d85777ec3eb622
Dmyto Kozubenko <kozubenko.dmitriy@gmail.com>
2023-02-07 22:30:29 +0100

    Added file conftest, and move fixtures to conftest

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,18 +1,3 @@
-import pytest
-
-
-class User:
-
-    def __init__(self) -> None:
-        self.name = "Dmytro"
-        self.second_name = "Kozubenko"
-
-
-@pytest.fixture
-def user():
-    yield User()
-
-
 def test_remove_name(user):
     user.name = ''
     assert user.name == ''
--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,30 +1,3 @@
-import pytest
-
-
-class User:
-
-    def __init__(self) -> None:
-        self.name = None
-        self.second_name = None
-
-    def create(self):
-        self.name = "Dmytro"
-        self.second_name = "Kozubenko"
-
-    def remove(self):
-        self.name = ""
-        self.second_name = ""
-
-
-@pytest.fixture
-def user():
-    user = User()
-    user.create()
-
-    yield user
-
-    user.remove()
-
 def test_change_name(user):
     assert user.name == 'Dmytro'
 
