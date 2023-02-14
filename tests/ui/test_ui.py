6d4f0082ba8109aae95b2ad10c3264fdb90931a4
Dmyto Kozubenko <kozubenko.dmitriy@gmail.com>
2023-02-14 22:57:08 +0100

    Mod test UI, login to github.com with pause. v109

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -2,7 +2,8 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By
+import time
 
 @pytest.mark.ui
 def test_check_incorrect_username():
@@ -13,6 +14,29 @@ def test_check_incorrect_username():
 
     # відкриваємо сторінку https://github.com/login 
     driver.get("https://github.com/login")
+
+    # Знаходимо поле, в яке будемо вводити неправильне ім'я користувача або поштову адресу
+    login_elem = driver.find_element(By.ID, "login_field")
+
+    # Вводимо неправильне ім'я користувача або поштову адрІесу
+    login_elem.send_keys("sergiibutenko@mistakeinemail.com")
+
+    # Знаходимо поле, в яке будемо вводити неправильний пароль
+    pass_elem = driver.find_element(By.ID, "password")
+
+    # Вводимо неправильний пароль
+    pass_elem.send_keys("wrong password")
+
+    # Знаходимо кнопку sign in
+    btn_elem = driver.find_element(By.NAME, "commit")
+
+    # Емулюємо клік лівою кнопкою мишки
+    btn_elem.click()
+
+    # Перевіряємо, що назва сторінки така, яку ми очікуємо
+    assert driver.title == "Sign in to GitHub · GitHub"
+
+    time.sleep(3)
 
     # Закриваємо браузер
     driver.close()
